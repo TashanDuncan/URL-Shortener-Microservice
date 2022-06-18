0541084ef213352d737c67e51bd7d451686e25d8
viewtifult <tashanduncan1994@gmail.com>
2022-06-18 19:22:51 +0100

    get request url

--- index.js
+++ index.js
@@ -16,12 +16,13 @@ const UrlSchema = new mongoose.Schema({
   },
   short_url: {
     type: Number,
+    default: 0,
   },
 });
 
 UrlSchema.plugin(AutoIncrement, { id: 'order_seq', inc_field: 'short_url' });
 
-const Url = mongoose.model('Url', UrlSchema)
+const Url = mongoose.model('Url', UrlSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -48,21 +49,33 @@ app.get('/api/hello', function (req, res) {
 });
 
 app.post('/api/shorturl', function (req, res) {
+  const { url: reqUrl } = req.body;
 
-  const {url: reqUrl} = req.body;
-
-  const validUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
-  if(!validUrl.test(reqUrl)){
-    return res.json({error: 'invalid url'})
+  const validUrl =
+    /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
+  if (!validUrl.test(reqUrl)) {
+    return res.json({ error: 'invalid url' });
   }
   let newUrl = new Url({
-    original_url: reqUrl
-  })
-  newUrl.save((err, data) =>{
+    original_url: reqUrl,
+  });
+  newUrl.save((err, data) => {
     if (err) return console.error(err);
-    return res.json( {original_url: data["original_url"], short_url: data["short_url"]});
-  })
-  
+    return res.json({
+      original_url: data['original_url'],
+      short_url: data['short_url'],
+    });
+  });
+});
+
+app.get('/api/shorturl/:short_url', function (req, res) {
+  const reqUrl = Number(req.params.short_url);
+
+  Url.findOne({ short_url: reqUrl }, function (err, data) {
+    if (err) return console.error(err);
+    console.log(data);
+    return res.redirect(data['original_url']);
+  });
 });
 
 app.listen(port, function () {
