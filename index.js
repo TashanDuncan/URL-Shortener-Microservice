1cb5106b57bb9f942a161f459f9c17cb8a733a4e
viewtifult <tashanduncan1994@gmail.com>
2022-06-18 18:34:40 +0100

    add short_url increment to DB

--- index.js
+++ index.js
@@ -2,23 +2,62 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const mySecret = process.env['MONGO_URI'];
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+mongoose.connect(mySecret, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const UrlSchema = new mongoose.Schema({
+  original_url: {
+    type: String,
+    required: [true, 'Must provide a URL'],
+  },
+  short_url: {
+    type: Number,
+  },
+});
+
+UrlSchema.plugin(AutoIncrement, { id: 'order_seq', inc_field: 'short_url' });
+
+const Url = mongoose.model('Url', UrlSchema)
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded());
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+//
+
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.post('/api/shorturl', function (req, res) {
+  let newUrl = new Url({
+    original_url: req.body.url
+  })
+  newUrl.save((err, data) =>{
+    if (err) return console.error(err);
+    return res.json( {original_url: data["original_url"], short_url: data["short_url"]});
+  })
+  
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
